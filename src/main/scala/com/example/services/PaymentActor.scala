844a77a84453bc6fd4ecbce3997e5caa1700f157
Billy Autrey <Billy.Autrey@lightbend.com>
2020-11-11 12:34:17 -0600

    Implemented PaymentActor

--- src/main/scala/com/example/services/PaymentActor.scala
+++ src/main/scala/com/example/services/PaymentActor.scala
@@ -1,5 +1,27 @@
 package com.example.services
 
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import com.example.CborSerializable
+
 object PaymentActor {
+
+  case class Amount(dollars: Int, cents: Int) extends CborSerializable
+  case class ProcessPayment(amount: Amount, orderId: String, ref: ActorRef[PaymentResponse]) extends CborSerializable
+
+  sealed trait PaymentResponse extends CborSerializable
+  case class PaymentSucceeded(amount: Amount, orderId: String) extends PaymentResponse
+  case class PaymentFailed(orderId: String, reason: String) extends PaymentResponse
+
+  def apply(): Behavior[ProcessPayment] = Behaviors.receiveMessage{
+    msg =>
+      msg.amount match {
+        case Amount(dollars, cents) if dollars > 500 =>
+          msg.ref ! PaymentFailed(msg.orderId, "Payment amount is too high.  Must be < $500")
+        case amount =>
+          msg.ref ! PaymentSucceeded(amount, msg.orderId)
+      }
+      Behaviors.same
+  }
 
 }
--- src/test/scala/com/example/services/PaymentActorSpec.scala
+++ src/test/scala/com/example/services/PaymentActorSpec.scala
@@ -1,7 +1,27 @@
 package com.example.services
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class PaymentActorSpec extends ScalaTestWithActorTestKit {
+class PaymentActorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike{
+
+  import PaymentActor._
+
+  "PaymentActor" should {
+    "respond PaymentSucceeded for a small payment" in {
+      val amount = Amount(100,0)
+      val paymentActor = testKit.spawn(PaymentActor())
+      val probe = testKit.createTestProbe[PaymentResponse]()
+      paymentActor ! ProcessPayment(amount, "123", probe.ref)
+      probe.expectMessage(PaymentSucceeded(amount,"123"))
+    }
+    "respond PaymentFailed for a large payment" in {
+      val amount = Amount(1000,0)
+      val paymentActor = testKit.spawn(PaymentActor())
+      val probe = testKit.createTestProbe[PaymentResponse]()
+      paymentActor ! ProcessPayment(amount, "123", probe.ref)
+      probe.expectMessage(PaymentFailed("123","Payment amount is too high.  Must be < $500"))
+    }
+  }
 
 }
