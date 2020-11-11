8cf0e1ff13b6e25de49f5d9b5178a707205ef0c5
Billy Autrey <Billy.Autrey@lightbend.com>
2020-11-11 12:01:12 -0600

    Implemented InventoryActor

--- src/main/scala/com/example/services/InventoryActor.scala
+++ src/main/scala/com/example/services/InventoryActor.scala
@@ -1,5 +1,50 @@
 package com.example.services
 
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import com.example.CborSerializable
+
+import scala.util.Try
+
 object InventoryActor {
+
+  case class State(items: Map[String, Int]) extends CborSerializable
+  case class ReserveItem(orderId: String, itemId: String, quantity: Int, ref: ActorRef[ReservationResponse]) extends CborSerializable
+  sealed trait ReservationResponse extends CborSerializable
+  case class ReservationMade(orderId: String, itemId: String, quantity: Int) extends ReservationResponse
+  case class ReservationFailed(orderId: String, itemId: String, quantity: Int, cause: String) extends ReservationResponse
+
+  //This is not a good pattern.  For demo purposes, however, we will just create a hard-coded inventory
+  val inventory = Map(
+    "socks" -> 10,
+    "shoes" -> 15,
+    "pants" -> 3,
+    "shirt" -> 7,
+    "backpack" -> 2
+  )
+
+  def apply(): Behavior[ReserveItem] = Behaviors.setup{
+    context =>
+    receive(context, State(inventory))
+  }
+
+  def receive(context: ActorContext[ReserveItem], state: State): Behavior[ReserveItem] = Behaviors.receiveMessage{
+    msg =>
+      val orderId = msg.orderId
+      val itemId = msg.itemId
+      val quantity = msg.quantity
+      val maybeInventory = Try(state.items(itemId))
+
+      if(maybeInventory.isFailure){
+        msg.ref ! ReservationFailed(orderId, itemId, quantity, s"ItemId '$itemId' was not found in inventory")
+        Behaviors.same
+      }else if(maybeInventory.getOrElse(0) >= quantity){
+        msg.ref ! ReservationMade(orderId, itemId, quantity)
+        receive(context, State(state.items + (itemId -> (maybeInventory.get - quantity))))
+      } else {
+        msg.ref ! ReservationFailed(orderId, itemId, quantity, "Not enough inventory")
+        Behaviors.same
+      }
+  }
 
 }
--- src/test/scala/com/example/services/InventoryActorSpec.scala
+++ src/test/scala/com/example/services/InventoryActorSpec.scala
@@ -1,7 +1,31 @@
 package com.example.services
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class InventoryActorSpec extends ScalaTestWithActorTestKit {
+class InventoryActorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  "InventoryActor" should {
+    "respond ReservationMade for a valid item and quantity" in {
+      val inventoryActor = testKit.spawn(InventoryActor())
+      val probe = testKit.createTestProbe[InventoryActor.ReservationResponse]()
+      inventoryActor ! InventoryActor.ReserveItem("123", "socks", 1, probe.ref)
+      probe.expectMessage(InventoryActor.ReservationMade("123","socks", 1))
+    }
+
+    "respond ReservationFailed for an invalid item" in {
+      val inventoryActor = testKit.spawn(InventoryActor())
+      val probe = testKit.createTestProbe[InventoryActor.ReservationResponse]()
+      inventoryActor ! InventoryActor.ReserveItem("123", "bacon", 1, probe.ref)
+      probe.expectMessage(InventoryActor.ReservationFailed("123","bacon", 1, "ItemId 'bacon' was not found in inventory"))
+    }
+
+    "respond ReservationFailed for an invalid quantity" in {
+      val inventoryActor = testKit.spawn(InventoryActor())
+      val probe = testKit.createTestProbe[InventoryActor.ReservationResponse]()
+      inventoryActor ! InventoryActor.ReserveItem("123", "socks", 100, probe.ref)
+      probe.expectMessage(InventoryActor.ReservationFailed("123","socks", 100, "Not enough inventory"))
+    }
+  }
 
 }
