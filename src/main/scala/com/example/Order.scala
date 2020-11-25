60c829c88c0b005dfce56160f155365a003ea5ba
Billy Autrey <Billy.Autrey@lightbend.com>
2020-11-25 11:42:34 -0600

    Simple compensating actions, without timeouts

--- src/main/scala/com/example/Order.scala
+++ src/main/scala/com/example/Order.scala
@@ -1,4 +1,7 @@
 package com.example
 
 case class Order(orderId: String, item: (String, Int))
+object Order{
+  val empty: Order = Order("", ("",0))
+}
 
--- src/main/scala/com/example/TypedSagaActor.scala
+++ src/main/scala/com/example/TypedSagaActor.scala
@@ -8,10 +8,11 @@ object TypedSagaActor {
 
   case class TypeState(
                         orderId: String,
+                        order: Option[Order],
                         emailService: ActorRef[EmailServiceActor.EmailCommand],
                         inventory: ActorRef[InventoryActor.InventoryCommand],
                         payment: ActorRef[PaymentActor.PaymentCommand],
-                        inventoryResponseMapper: ActorRef[InventoryActor.ReservationResponse],
+                        inventoryResponseMapper: ActorRef[InventoryActor.InventoryResponse],
                         emailResponseMapper: ActorRef[EmailServiceActor.EmailServiceResponse],
                         paymentResponseWrapper: ActorRef[PaymentActor.PaymentResponse]
                       ) extends CborSerializable
@@ -24,7 +25,7 @@ object TypedSagaActor {
   // Adapted responses - https://doc.akka.io/docs/akka/current/typed/interaction-patterns.html#adapted-response
   private final case class WrappedEmailServiceResponse(response: EmailServiceActor.EmailServiceResponse)
     extends SagaCommand
-  private final case class WrappedInventoryResponse(response: InventoryActor.ReservationResponse) extends SagaCommand
+  private final case class WrappedInventoryResponse(response: InventoryActor.InventoryResponse) extends SagaCommand
   private final case class WrappedPaymentResponse(response: PaymentActor.PaymentResponse) extends SagaCommand
 
   def apply(
@@ -37,6 +38,7 @@ object TypedSagaActor {
       ready(
         TypeState(
           orderId,
+          None,
           emailService,
           inventory,
           payment,
@@ -52,7 +54,7 @@ object TypedSagaActor {
     case ProcessTransaction(order) =>
       context.log.info(s"Processing order ${order.orderId}")
       state.inventory ! InventoryActor.ReserveItem(order, state.inventoryResponseMapper)
-      waitingOnInventoryResponse(state, context)
+      waitingOnInventoryResponse(state.copy(order = Some(order)), context)
     case msg =>
       context.log.info(s"Unexpected message $msg in ready state")
       Behaviors.same
@@ -61,15 +63,15 @@ object TypedSagaActor {
   def waitingOnInventoryResponse(state: TypeState, context: ActorContext[_]): Behavior[SagaCommand] = Behaviors.receiveMessage{
     case WrappedInventoryResponse(msg) =>
       msg match {
-        case InventoryActor.ReservationMade(orderId, itemId, quantity) =>
-          context.log.info(s"Received item reservation for $quantity $itemId")
-          state.payment ! PaymentActor.ProcessPayment(PaymentActor.Amount(1,99),orderId, state.paymentResponseWrapper)
+        case InventoryActor.ReservationMade(order) =>
+          context.log.info(s"Received item reservation for ${order.item._2} ${order.item._1}")
+          state.payment ! PaymentActor.ProcessPayment(PaymentActor.Amount(1,99),order.orderId, state.paymentResponseWrapper)
           waitingOnPaymentResponse(state, context)
-        case InventoryActor.ReservationFailed(orderId, itemId, quantity, cause) =>
-          context.log.error(s"Transaction $orderId failed - $cause")
+        case InventoryActor.ReservationFailed(order, cause) =>
+          context.log.error(s"Transaction ${order.orderId} failed, rolling back transaction.  Cause: $cause")
           // take compensating action, and un-reserve item
-          state.inventory ! InventoryActor.UnreserveItem(Order(orderId, (itemId, quantity)), state.inventoryResponseMapper)
-          Behaviors.same
+          state.inventory ! InventoryActor.UnreserveItem(order, state.inventoryResponseMapper)
+          waitingOnInventoryCompensatingAction(state, context)
       }
     case msg =>
       context.log.warn(s"State: WaitingOnInventoryResponse.  Unexpected message: $msg")
@@ -83,10 +85,11 @@ object TypedSagaActor {
           context.log.info(s"Payment for $orderId succeeded:  $amount")
           state.emailService ! EmailServiceActor.SendEmail("example@gmail.com", orderId, state.emailResponseMapper)
           ready(state, context)
-        case PaymentActor.PaymentFailed(orderId, reason) =>
+        case PaymentActor.PaymentFailed(orderId, amount, reason) =>
           context.log.error(s"Payment for $orderId failed:  $reason")
           //compensating action
-          Behaviors.same
+          state.payment ! PaymentActor.RefundPayment(amount, orderId, state.paymentResponseWrapper)
+          waitingOnPaymentCompensatingAction(state, context)
       }
     case msg =>
       context.log.warn(s"State: WaitingOnPaymentResponse.  Unexpected message:  $msg")
@@ -99,4 +102,41 @@ object TypedSagaActor {
     Behaviors.same
   }
 
+  //compensating action behaviors
+
+  def waitingOnInventoryCompensatingAction(state: TypeState, context: ActorContext[_]): Behavior[SagaCommand] = Behaviors.receiveMessage{
+    case WrappedInventoryResponse(response) =>
+      response match {
+        case InventoryActor.UnreserveFailed(order, reason) =>
+          context.log.error(s"Failure un-reserving ${order.orderId}, retrying.  Cause: $reason")
+          state.inventory ! InventoryActor.UnreserveItem(order, state.inventoryResponseMapper)
+          Behaviors.same
+        case InventoryActor.UnreserveSucceeded(orderId) =>
+          context.log.info("Rolled back inventory reservation, going to ready state")
+          ready(state,context)
+        case msg =>
+          context.log.warn(s"State: WaitingOnInventoryCompensatingAction.  Unexpected message: $msg")
+          Behaviors.same
+      }
+    case msg =>
+      context.log.warn(s"State: WaitingOnInventoryCompensatingAction.  Unexpected message: $msg")
+      Behaviors.same
+  }
+
+  def waitingOnPaymentCompensatingAction(state: TypeState, context: ActorContext[_]): Behavior[SagaCommand] = Behaviors.receiveMessage{
+    case WrappedPaymentResponse(response) =>
+      response match {
+        case PaymentActor.RefundFailed(amount, orderId, cause) =>
+          Behaviors.same
+        case PaymentActor.RefundSucceeded(amount, orderId) =>
+          state.inventory ! InventoryActor.UnreserveItem(state.order.getOrElse(Order.empty.copy(orderId = orderId)), state.inventoryResponseMapper)
+          waitingOnInventoryCompensatingAction(state, context)
+        case paymentMsg =>
+          context.log.warn(s"State: WaitingOnPaymentCompensatingAction.  Unexpected message: $paymentMsg")
+          Behaviors.same
+      }
+    case msg =>
+      context.log.warn(s"State: WaitingOnPaymentCompensatingAction.  Unexpected message: $msg")
+      Behaviors.same
+  }
 }
