cc43aef4dc4ce0b1e86280417522adf8df598092
Billy Autrey <Billy.Autrey@lightbend.com>
2020-12-08 15:49:40 -0600

    Event sourced saga examples

--- src/main/scala/com/example/EventSourcedSagaActor.scala
+++ src/main/scala/com/example/EventSourcedSagaActor.scala
@@ -1,47 +1,174 @@
 package com.example
 
-import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.persistence.typed.PersistenceId
 import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+import com.example.services.{EmailServiceActor, InventoryActor, PaymentActor}
 
 object EventSourcedSagaActor {
 
-  sealed trait State extends CborSerializable
-  object State{
-    val empty: State = ReadyState("")
+  sealed trait State extends CborSerializable {
+    def id: String
+    def refs: Refs
   }
 
-  case class ReadyState(id: String) extends State
-  case class WaitingOnInventoryState(id: String) extends State
-  case class WaitingOnPaymentState(id: String) extends State
+  case class Refs(
+                   inventoryActor: ActorRef[InventoryActor.InventoryCommand],
+                   emailServiceActor: ActorRef[EmailServiceActor.EmailCommand],
+                   paymentActor: ActorRef[PaymentActor.PaymentCommand],
+                   inventoryResponseMapper: Option[ActorRef[InventoryActor.InventoryResponse]],
+                   emailResponseMapper: Option[ActorRef[EmailServiceActor.EmailServiceResponse]],
+                   paymentResponseWrapper: Option[ActorRef[PaymentActor.PaymentResponse]]
+                 ) extends CborSerializable
 
   sealed trait Command extends CborSerializable
+  sealed trait Event   extends CborSerializable
 
-  sealed trait Event extends CborSerializable
+  final case class ReadyState(id: String, refs: Refs)              extends State
+  final case class WaitingOnInventoryState(id: String, refs: Refs) extends State
+  final case class WaitingOnPaymentState(id: String, refs: Refs)   extends State
 
-  def apply(entityId: String): Behavior[Command] = Behaviors.setup {
-    context =>
-      EventSourcedBehavior[Command,Event, State](
-        persistenceId = PersistenceId("EventSourcedSaga",entityId),
-        emptyState = State.empty,
-        commandHandler = commandHandler(context),
-        eventHandler = eventHandler(context)
-      )
+  final case class ProcessTransaction(order: Order) extends Command
+  final case object SpanishInquisition              extends Command //Nobody expects the Spanish Inquisition!
+
+  final case class ProcessTransactionReceived(order: Order) extends Event
+  final case class ReservationMadeReceived(order: Order) extends Event
+
+  // Adapted responses - https://doc.akka.io/docs/akka/current/typed/interaction-patterns.html#adapted-response
+  private final case class WrappedEmailServiceResponse(response: EmailServiceActor.EmailServiceResponse) extends Command
+  private final case class WrappedInventoryResponse(response: InventoryActor.InventoryResponse) extends Command
+  private final case class WrappedPaymentResponse(response: PaymentActor.PaymentResponse) extends Command
+
+  def apply(
+             entityId: String,
+             inventoryActor: ActorRef[InventoryActor.InventoryCommand],
+             emailServiceActor: ActorRef[EmailServiceActor.EmailCommand],
+             paymentActor: ActorRef[PaymentActor.PaymentCommand]
+           ): Behavior[Command] = Behaviors.setup { context =>
+    val refs = refFactory(inventoryActor, emailServiceActor, paymentActor, context)
+
+    EventSourcedBehavior[Command, Event, State](
+      persistenceId = PersistenceId("EventSourcedSaga", entityId),
+      emptyState = ReadyState(entityId, refs),
+      commandHandler = commandHandler(context),
+      eventHandler = eventHandler(context)
+    )
   }
 
-  def commandHandler(context: ActorContext[_])(state: State, command: Command): Effect[Event, State] = {
+  private def refFactory(
+                          inventoryActor: ActorRef[InventoryActor.InventoryCommand],
+                          emailServiceActor: ActorRef[EmailServiceActor.EmailCommand],
+                          paymentActor: ActorRef[PaymentActor.PaymentCommand],
+                          context: ActorContext[Command]
+                        ): Refs =
+    Refs(
+      inventoryActor,
+      emailServiceActor,
+      paymentActor,
+      Some(context.messageAdapter(rsp => WrappedInventoryResponse(rsp))),
+      Some(context.messageAdapter(rsp => WrappedEmailServiceResponse(rsp))),
+      Some(context.messageAdapter(rsp => WrappedPaymentResponse(rsp)))
+    )
+
+  def commandHandler(context: ActorContext[Command])(state: State, command: Command): Effect[Event, State] = {
     state match {
-      case WaitingOnInventoryState(id) =>
-        waitingOnInventoryCommandHandler(state, command)
-      case WaitingOnPaymentState(id) =>
+      case WaitingOnInventoryState(id, refs) =>
+        waitingOnInventoryCommandHandler(context)(state, command)
+      case WaitingOnPaymentState(id, refs) =>
         waitingOnPaymentCommandHandler(state, command)
+      case ready: ReadyState =>
+        readyCommandHandler(context)(ready, command)
     }
   }
 
-  def waitingOnInventoryCommandHandler(state: State, command: Command): Effect[Event, State] = Effect.noReply
+  def readyCommandHandler(context: ActorContext[Command])(state: ReadyState, command: Command): Effect[Event, State] = {
+    command match {
+      case ProcessTransaction(order) =>
+        Effect
+          .persist(ProcessTransactionReceived(order))
+          .thenRun(_ =>
+            state.refs.inventoryActor ! InventoryActor.ReserveItem(order, state.refs.inventoryResponseMapper.get)
+          )
+      case _ =>
+        context.log.warn(s"Unexpected message in 'Ready' state:  $command")
+        Effect.noReply
+    }
+  }
+
+  def waitingOnInventoryCommandHandler(context: ActorContext[Command])(state: State, command: Command): Effect[Event, State] = {
+    command match {
+      case WrappedInventoryResponse(response) =>
+        response match {
+          case InventoryActor.ReservationMade(order) =>
+            Effect
+              .persist(ReservationMadeReceived(order))
+              .thenRun( _ =>
+                state.refs.paymentActor ! PaymentActor.ProcessPayment(PaymentActor.Amount(1,99),order.orderId, state.refs.paymentResponseWrapper.get)
+              )
+          case InventoryActor.ReservationFailed(order, cause) =>
+            context.log.error(s"Unable to process ${order.orderId}: $cause")
+            Effect.noReply
+        }
+      case _ =>
+        context.log.warn(s"Unexpected message in 'WaitingOnInventory' state:  $command")
+        Effect.noReply
+    }
+  }
+
   def waitingOnPaymentCommandHandler(state: State, command: Command): Effect[Event, State] = Effect.noReply
 
-  def eventHandler(context: ActorContext[_])(state: State, event: Event): State = State.empty
+  def eventHandler(context: ActorContext[Command])(state: State, event: Event): State = {
+    state match {
+      case WaitingOnInventoryState(id, refs) =>
+        waitingOnInventoryEventHandler(state, event)
+      case WaitingOnPaymentState(_, _) =>
+        waitingOnPaymentEventHandler(state, event)
+      case ReadyState(id, refs) =>
+        readyEventHandler(state, event)
+    }
+  }
+
+  def readyEventHandler(state: State, event: Event): State = event match {
+    case ProcessTransactionReceived(order) =>
+      WaitingOnInventoryState(state.id, state.refs)
+    case _ =>
+      state
+  }
+
+  // Stubbed, for now
+  def waitingOnInventoryEventHandler(state: State, event: Event): State = event match {
+    case ReservationMadeReceived(order) =>
+      WaitingOnPaymentState(state.id, state.refs)
+    case _ =>
+      state
+  }
+
+  def waitingOnPaymentEventHandler(state: State, event: Event): State = state
+
+  // For testing
+  def apply(partialState: State): Behavior[Command] = Behaviors.setup { context =>
+    val refs = partialState.refs match {
+      case Refs(i, e, p, None, None, None) =>
+        refFactory(i, e, p, context)
+      case valid =>
+        valid
+    }
+
+    val state = partialState match {
+      case r: ReadyState => r.copy(refs = refs)
+      case i: WaitingOnInventoryState => i.copy(refs = refs)
+      case p: WaitingOnPaymentState => p.copy(refs = refs)
+    }
+
+    context.log.info(s"Starting with ${state.getClass}")
+
+    EventSourcedBehavior[Command, Event, State](
+      persistenceId = PersistenceId("EventSourcedSaga", state.id),
+      emptyState = state,
+      commandHandler = commandHandler(context),
+      eventHandler = eventHandler(context)
+    )
+  }
 
 }
--- src/test/scala/com/example/EventSourcedSagaActorSpec.scala
+++ src/test/scala/com/example/EventSourcedSagaActorSpec.scala
@@ -1,8 +1,160 @@
 package com.example
 
-import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.actor.testkit.typed.scaladsl.{LogCapturing, LoggingTestKit, ScalaTestWithActorTestKit, TestProbe}
 import akka.persistence.testkit.scaladsl.EventSourcedBehaviorTestKit
+import com.example.services.PaymentActor.Amount
+import com.example.services.{EmailServiceActor, InventoryActor, PaymentActor}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class EventSourcedSagaActorSpec extends ScalaTestWithActorTestKit(EventSourcedBehaviorTestKit.config) {
+class EventSourcedSagaActorSpec
+  extends ScalaTestWithActorTestKit(
+    ConfigFactory.parseString(
+      """
+        |akka.actor.serialization-bindings {
+        |  "com.example.CborSerializable" = jackson-cbor
+        |}
+        |""".stripMargin)
+      .withFallback(EventSourcedBehaviorTestKit.config)
+  )
+  with AnyWordSpecLike
+  with BeforeAndAfterEach
+  with LogCapturing {
+  import EventSourcedSagaActor._
 
+  val orderId = "123"
+
+  class EventSourcedSagaActor(id: String)
+
+  private def eventSourcedTestKit(state: State) = EventSourcedBehaviorTestKit[Command, Event, State](
+    system,
+    EventSourcedSagaActor(state)
+  )
+
+  private def testRefsFactory(
+                emailServiceActor: TestProbe[EmailServiceActor.EmailCommand],
+                inventoryActor: TestProbe[InventoryActor.InventoryCommand],
+                paymentActor: TestProbe[PaymentActor.PaymentCommand]
+              ): Refs = {
+    Refs(inventoryActor.ref, emailServiceActor.ref, paymentActor.ref, None, None, None)
+  }
+
+  override protected def beforeEach(): Unit = {
+    super.beforeEach()
+  }
+
+  "EventSourcedSagaActor" should {
+    "given ready state, when receiving a ProcessTransaction, send ReserveItem to InventoryActor" in {
+      val itemId = "socks"
+      val quantity = 1
+      val order = Order(orderId, (itemId, quantity))
+      val emailServiceActor = testKit.createTestProbe[EmailServiceActor.EmailCommand]("emailServiceActor")
+      val inventoryActor = testKit.createTestProbe[InventoryActor.InventoryCommand]("inventoryActor")
+      val paymentActor = testKit.createTestProbe[PaymentActor.PaymentCommand]("paymentActor")
+      val refs = testRefsFactory(emailServiceActor, inventoryActor, paymentActor)
+
+      // Create the actor, and process a transaction
+      val eventSourcedSagaActor = eventSourcedTestKit(EventSourcedSagaActor.ReadyState(itemId, refs))
+      val result = eventSourcedSagaActor.runCommand(ProcessTransaction(order))
+
+      // Inventory actor should have received a reservation
+      val msg = inventoryActor.expectMessageType[InventoryActor.ReserveItem]
+      msg.order.orderId shouldBe "123"
+      msg.order.item._2 shouldBe 1
+
+      // EventSourcedSagaActor should have transitioned state, and triggered the proper event
+      eventSourcedSagaActor.getState() shouldBe a [EventSourcedSagaActor.WaitingOnInventoryState]
+      result.event === ProcessTransaction(order)
+
+      //cleanup
+      eventSourcedSagaActor.clear()
+    }
+
+    "given ready state, when receiving an unexpected message, log an unexpected message" in {
+      val itemId = "socks"
+      val quantity = 1
+      val order = Order(orderId, (itemId, quantity))
+      val emailServiceActor = testKit.createTestProbe[EmailServiceActor.EmailCommand]("emailServiceActor")
+      val inventoryActor = testKit.createTestProbe[InventoryActor.InventoryCommand]("inventoryActor")
+      val paymentActor = testKit.createTestProbe[PaymentActor.PaymentCommand]("paymentActor")
+      val refs = testRefsFactory(emailServiceActor, inventoryActor, paymentActor)
+
+      // Create the actor, and process a transaction
+      val eventSourcedSagaActor = eventSourcedTestKit(EventSourcedSagaActor.ReadyState(itemId, refs))
+
+      //expect the unexpected
+      LoggingTestKit.warn("SpanishInquisition").expect{
+        val result = eventSourcedSagaActor.runCommand(SpanishInquisition)
+
+        result.hasNoEvents shouldBe true
+      }
+
+      // Inventory actor should NOT have received a reservation
+      inventoryActor.expectNoMessage()
+
+      // EventSourcedSagaActor should NOT have transitioned state
+      eventSourcedSagaActor.getState() shouldBe a [EventSourcedSagaActor.ReadyState]
+
+      //cleanup
+      eventSourcedSagaActor.clear()
+    }
+
+    "given WaitingOnInventory state, when receiving an inventory response, send a message to process payment" in {
+      val itemId = "socks"
+      val quantity = 1
+      val order = Order(orderId, (itemId, quantity))
+      val emailServiceActor = testKit.createTestProbe[EmailServiceActor.EmailCommand]("emailServiceActor")
+      val inventoryActor = testKit.createTestProbe[InventoryActor.InventoryCommand]("inventoryActor")
+      val paymentActor = testKit.createTestProbe[PaymentActor.PaymentCommand]("paymentActor")
+      val refs = testRefsFactory(emailServiceActor, inventoryActor, paymentActor)
+
+      // Create the actor, and process a transaction
+      val eventSourcedSagaActor = eventSourcedTestKit(EventSourcedSagaActor.WaitingOnInventoryState(itemId, refs))
+      val inventoryResponseMapper = eventSourcedSagaActor.getState().refs.inventoryResponseMapper.get //gross
+      inventoryResponseMapper ! InventoryActor.ReservationMade(order) // This acts like a mapper for the behavior I care about
+      //val result = eventSourcedSagaActor.runCommand(WrappedInventoryResponse(InventoryActor.ReservationMade(order))) //wish I could do this, to get events
+
+      // Inventory actor should have received a reservation
+      val msg = paymentActor.expectMessageType[PaymentActor.ProcessPayment]
+      msg.amount shouldBe Amount(1,99)
+
+      // EventSourcedSagaActor should have transitioned state, and triggered the proper event
+      eventSourcedSagaActor.getState() shouldBe a [EventSourcedSagaActor.WaitingOnPaymentState]
+      //result.event === ProcessTransaction(order) //I can't get at the events now, to assert on them
+
+      //cleanup
+      eventSourcedSagaActor.clear()
+    }
+
+    "given awaitingInventoryResponse, when receiving an unexpected command, log an unexpected message warning" in {
+      val itemId = "socks"
+      val quantity = 1
+      val order = Order(orderId, (itemId, quantity))
+      val emailServiceActor = testKit.createTestProbe[EmailServiceActor.EmailCommand]("emailServiceActor")
+      val inventoryActor = testKit.createTestProbe[InventoryActor.InventoryCommand]("inventoryActor")
+      val paymentActor = testKit.createTestProbe[PaymentActor.PaymentCommand]("paymentActor")
+      val refs = testRefsFactory(emailServiceActor, inventoryActor, paymentActor)
+
+      // Create the actor, and process a transaction
+      val eventSourcedSagaActor = eventSourcedTestKit(EventSourcedSagaActor.WaitingOnInventoryState(itemId, refs))
+
+      //expect the unexpected
+      LoggingTestKit.warn("SpanishInquisition").expect{
+        val result = eventSourcedSagaActor.runCommand(SpanishInquisition)
+
+        result.hasNoEvents shouldBe true
+      }
+
+      // Inventory actor should NOT have received a reservation
+      paymentActor.expectNoMessage()
+
+      // EventSourcedSagaActor should NOT have transitioned state
+      eventSourcedSagaActor.getState() shouldBe a [EventSourcedSagaActor.WaitingOnInventoryState]
+
+      //cleanup
+      eventSourcedSagaActor.clear()
+    }
+
+  }
 }
