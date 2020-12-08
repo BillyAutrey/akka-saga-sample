b9caa04138735ced5c0644cc5882d414ea562b67
Billy Autrey <Billy.Autrey@lightbend.com>
2020-12-08 15:49:14 -0600

    Making orders serializable, for testing

--- src/main/scala/com/example/Order.scala
+++ src/main/scala/com/example/Order.scala
@@ -1,6 +1,6 @@
 package com.example
 
-case class Order(orderId: String, item: (String, Int))
+case class Order(orderId: String, item: (String, Int)) extends CborSerializable
 object Order{
   val empty: Order = Order("", ("",0))
 }
